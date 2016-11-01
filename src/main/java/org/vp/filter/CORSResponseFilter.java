38e2713999775657b1cc314d52f29296afa4702c
matiur.rahman <mrahman@fuzzproductions.com>
2016-11-01 03:15:42 -0400

    cors

--- src/main/java/org/vp/filter/CORSResponseFilter.java
+++ src/main/java/org/vp/filter/CORSResponseFilter.java
@@ -18,7 +18,7 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 
         headers.add("Access-Control-Allow-Origin", "*");
         //headers.add("Access-Control-Allow-Origin", "http://podcastpedia.org"); //allows CORS requests only coming from podcastpedia.org
-        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
         headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept,Authorization, X-Requested-With," +
                 "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Requested-With, " +
                 "Content-Type, X-Codingpedia");
