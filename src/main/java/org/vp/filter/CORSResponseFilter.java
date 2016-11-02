9c208e8750112822cf6596e59ad189a96a66a0c8
matiur.rahman <mrahman@fuzzproductions.com>
2016-11-01 23:23:43 -0400

    cors

--- src/main/java/org/vp/filter/CORSResponseFilter.java
+++ src/main/java/org/vp/filter/CORSResponseFilter.java
@@ -11,12 +11,13 @@ import java.io.IOException;
  */
 public class CORSResponseFilter implements ContainerResponseFilter {
 
+    @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
-        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Origin", "http://ec2-54-152-87-100.compute-1.amazonaws.com:8080");
         //headers.add("Access-Control-Allow-Origin", "http://podcastpedia.org"); //allows CORS requests only coming from podcastpedia.org
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
         headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept,Authorization, X-Requested-With," +
