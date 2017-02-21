532a8f25495a0a5534cd2e983a2a5c71605d25e7
matiur rahman <mrahman@fuzzproductions.com>
2017-02-21 12:00:37 -0500

    user-profile

--- src/main/java/org/vp/vc/profile/VCProfile.java
+++ src/main/java/org/vp/vc/profile/VCProfile.java
@@ -22,12 +22,16 @@ public class VCProfile {
 
     }
 
+    public VCProfile(VCInfo vcInfo, SocialData socialData) {
+        this.vcInfo = vcInfo;
+        this.socialData = socialData;
+    }
+
     public VCProfile(VCInfo vcInfo, SocialData socialData, List<FundingHistory> fundingHistory) {
         this.vcInfo = vcInfo;
         this.socialData = socialData;
         this.fundingHistory = fundingHistory;
     }
-
 
     public VCProfile(String id,VCInfo vcInfo, SocialData socialData, List<FundingHistory> fundingHistory) {
         this._id = id;
